discordia-space/CEV-Eris

Breaks up autolathe_disks.dm (#5842)
* Breaks up autolathe_disks.dm

* _

code/game/objects/items/weapons/design_disks/ironhammer.dm
@@ -1,0 +1,75 @@
+// Ironhammer
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/security
+	disk_name = "Ironhammer Miscellaneous Pack"
+	icon_state = "ironhammer"
+	rarity_value = 5
+	license = 20
+	designs = list(
+		/datum/design/autolathe/sec/secflashlight,
+		/datum/design/research/item/flash,
+		/datum/design/autolathe/sec/handcuffs,
+		/datum/design/autolathe/sec/zipties,
+		/datum/design/autolathe/sec/electropack,
+		/datum/design/autolathe/misc/taperecorder,
+		/datum/design/autolathe/tool/tacknife,
+		/datum/design/autolathe/sec/beartrap,
+		/datum/design/autolathe/sec/silencer,
+		/datum/design/autolathe/sec/hailer
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/generic
+	disk_name = "Ironhammer Combat Equipment - Standard Armor"
+	icon_state = "ironhammer"
+	spawn_tags = SPAWN_TAG_DESING_ADVANCED_COMMON
+	rarity_value = 12 // one of the more common advanced disks
+	license = 6 // 6 pieces, or 3 sets if you use helm + vest
+	designs = list(
+		/datum/design/autolathe/clothing/generic_helmet_basic,
+		/datum/design/autolathe/clothing/generic_vest,
+		/datum/design/autolathe/clothing/generic_vest_full = 2
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/generic/bulletproof
+	disk_name = "Ironhammer Combat Equipment - Bulletproof Armor"
+	icon_state = "ironhammer"
+	spawn_tags = SPAWN_TAG_DESING_ADVANCED_COMMON
+	rarity_value = 15 // about as rare as a advanced tool disk - remember that this takes from the 'advanced' pool (which is rare) instead of the 'common' pool like the normal armor disk does
+	license = 4 // 4 pieces, or 2 sets
+	designs = list(
+		/datum/design/autolathe/clothing/bulletproof_helmet_generic,
+		/datum/design/autolathe/clothing/bulletproof_vest_generic,
+		/datum/design/autolathe/clothing/bulletproof_vest_generic_full = 2
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/generic/ablative
+	disk_name = "Ironhammer Combat Equipment - Laserproof Armor"
+	icon_state = "ironhammer"
+	spawn_tags = SPAWN_TAG_DESING_ADVANCED_COMMON
+	rarity_value = 16 // slightly rarer than bulletproof gear
+	license = 4 // 4 pieces, or 2 sets
+	designs = list(
+		/datum/design/autolathe/clothing/ablative_vest,
+		/datum/design/autolathe/clothing/ablative_helmet
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/ih
+	disk_name = "Ironhammer Combat Equipment - Operator Armor"
+	icon_state = "ironhammer"
+	spawn_blacklisted = TRUE //should only be obtainable from the sectech
+	license = 6
+	designs = list(
+		/datum/design/autolathe/clothing/ih_helmet_basic,
+		/datum/design/autolathe/clothing/ih_vest_basic,
+		/datum/design/autolathe/clothing/ih_vest_basic_full = 2
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/ih/bulletproof
+	disk_name = "Ironhammer Combat Equipment - Bulletproof Operator Armor"
+	icon_state = "ironhammer"
+	spawn_blacklisted = TRUE
+	license = 4
+	designs = list(
+		/datum/design/autolathe/clothing/ih_helmet_full,
+		/datum/design/autolathe/clothing/ih_vest_full
+	)
+

code/game/objects/items/weapons/design_disks/moebius.dm
@@ -1,0 +1,83 @@
+// Moebius
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/medical
+	disk_name = "Moebius Medical Designs"
+	icon_state = "moebius"
+	rarity_value = 4.5
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	license = 20
+	designs = list(
+		/datum/design/autolathe/gun/syringe_gun,
+		/datum/design/autolathe/misc/penflashlight,
+		/datum/design/autolathe/tool/scalpel,
+		/datum/design/autolathe/tool/circularsaw,
+		/datum/design/autolathe/tool/surgicaldrill,
+		/datum/design/autolathe/tool/retractor,
+		/datum/design/autolathe/tool/cautery,
+		/datum/design/autolathe/tool/hemostat,
+		/datum/design/autolathe/tool/bonesetter,
+		/datum/design/autolathe/container/syringe,
+		/datum/design/autolathe/container/vial,
+		/datum/design/autolathe/container/beaker,
+		/datum/design/autolathe/container/beaker_large,
+		/datum/design/autolathe/container/pill_bottle,
+		/datum/design/autolathe/container/spray,
+		/datum/design/autolathe/container/freezer_medical,
+		/datum/design/autolathe/device/implanter,
+		/datum/design/autolathe/container/syringegun_ammo,
+		/datum/design/autolathe/container/syringe/large,
+
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/surgery
+	disk_name = "Back Alley Organs"
+	icon_state = "moebius"
+	license = 5
+	designs = list(
+		/datum/design/bioprinter/lungs
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/computer
+	disk_name = "Moebius Computer Parts"
+	icon_state = "moebius"
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	rarity_value = 4.5
+	license = 20
+	designs = list(
+		/datum/design/autolathe/computer_part/frame_pda,
+		/datum/design/autolathe/computer_part/frame_tablet,
+		/datum/design/autolathe/computer_part/frame_laptop,
+		/datum/design/research/item/computer_part/disk/micro,
+		/datum/design/research/item/computer_part/disk/small,
+		/datum/design/research/item/computer_part/disk/normal,
+		/datum/design/research/item/computer_part/disk/advanced,
+		/datum/design/research/item/computer_part/cpu/basic,
+		/datum/design/research/item/computer_part/cpu/basic/small,
+		/datum/design/research/item/computer_part/cpu/adv,
+		/datum/design/research/item/computer_part/cpu/adv/small,
+		/datum/design/research/item/computer_part/netcard/basic,
+		/datum/design/research/item/computer_part/netcard/advanced,
+		/datum/design/research/item/computer_part/netcard/wired,
+		/datum/design/research/item/computer_part/cardslot,
+		/datum/design/research/item/computer_part/teslalink,
+		/datum/design/research/item/computer_part/portabledrive/basic,
+		/datum/design/research/item/computer_part/portabledrive/normal,
+		/datum/design/research/item/computer_part/printer,
+		/datum/design/research/item/computer_part/led,
+		/datum/design/research/item/computer_part/led/adv,
+		/datum/design/autolathe/computer_part/gps,
+		/datum/design/autolathe/computer_part/scanner/paper,
+		/datum/design/autolathe/computer_part/scanner/atmos,
+		/datum/design/autolathe/computer_part/scanner/reagent,
+		/datum/design/autolathe/computer_part/scanner/medical,
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/guns/ms_dartgun
+	disk_name = "Moebius Scientifica - Z-H P Artemis Dartgun"
+	icon_state = "moebius"
+	spawn_tags = SPAWN_TAG_DESING_ADVANCED
+	rarity_value = 50
+	license = 12
+	designs = list(
+		/datum/design/autolathe/gun/dart_gun = 3, // Z-H P Artemis"
+		/datum/design/autolathe/ammo/dart_mag,
+	)

code/game/objects/items/weapons/design_disks/technomancers.dm
@@ -1,0 +1,94 @@
+// Technomancers
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/components
+	disk_name = "Technomancers ARK-034 Components"
+	icon_state = "technomancers"
+	rarity_value = 4
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	license = 20
+	designs = list(
+		/datum/design/autolathe/part/consolescreen,
+		/datum/design/research/item/part/smes_coil,
+		/datum/design/research/item/part/basic_capacitor,
+		/datum/design/research/item/part/basic_sensor,
+		/datum/design/research/item/part/micro_mani,
+		/datum/design/research/item/part/basic_micro_laser,
+		/datum/design/research/item/part/basic_matter_bin,
+		/datum/design/autolathe/part/igniter,
+		/datum/design/autolathe/part/signaler,
+		/datum/design/autolathe/part/sensor_infra,
+		/datum/design/autolathe/part/timer,
+		/datum/design/autolathe/part/voice_analyzer,
+		/datum/design/autolathe/part/sensor_prox,
+		/datum/design/autolathe/part/camera_assembly,
+		/datum/design/autolathe/part/laserguide,
+		/datum/design/research/item/ducts
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/adv_tools
+	disk_name = "Technomancers IJIRO-451 Advanced Tools"
+	icon_state = "technomancers"
+	rarity_value = 11
+	spawn_tags = SPAWN_TAG_DESING_ADVANCED_COMMON
+	license = 10
+	designs = list(
+		/datum/design/autolathe/tool/big_wrench,
+		/datum/design/autolathe/tool/pneumatic_crowbar,
+		/datum/design/research/item/weapon/mining/jackhammer,
+		/datum/design/research/item/weapon/mining/drill,
+		/datum/design/research/item/weapon/mining/drill_diamond,
+		/datum/design/autolathe/tool/pickaxe_excavation,
+		/datum/design/autolathe/tool/shovel/power,
+		/datum/design/autolathe/tool/circularsaw,
+		/datum/design/autolathe/tool/powered_hammer,
+		/datum/design/autolathe/tool/chainsaw,
+		/datum/design/autolathe/tool/hypersaw,
+		/datum/design/autolathe/tool/rcd,
+		/datum/design/autolathe/tool/electric_screwdriver,
+		/datum/design/autolathe/tool/combi_driver,
+		/datum/design/autolathe/tool/armature_cutter,
+		/datum/design/autolathe/tool/weldertool_adv,
+		/datum/design/autolathe/part/diamondblade,
+		/datum/design/autolathe/tool/rpd,
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/circuits
+	disk_name = "Technomancers ESPO-830 Circuits"
+	icon_state = "technomancers"
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	rarity_value = 5.5
+	license = 10
+	designs = list(
+		/datum/design/autolathe/circuit/airlockmodule = 0,
+		/datum/design/autolathe/circuit/airlockmodule/secure,
+		/datum/design/autolathe/circuit/airalarm = 0,
+		/datum/design/autolathe/circuit/firealarm = 0,
+		/datum/design/autolathe/circuit/powermodule = 0,
+		/datum/design/autolathe/circuit/recharger,
+		/datum/design/research/circuit/autolathe,
+		/datum/design/autolathe/circuit/autolathe_disk_cloner = 3,
+		/datum/design/autolathe/circuit/vending,
+		/datum/design/research/circuit/arcade_battle,
+		/datum/design/research/circuit/arcade_orion_trail,
+		/datum/design/research/circuit/teleconsole,
+		/datum/design/research/circuit/operating,
+		/datum/design/autolathe/circuit/helm,
+		/datum/design/autolathe/circuit/nav,
+		/datum/design/autolathe/circuit/centrifuge,
+		/datum/design/autolathe/circuit/electrolyzer,
+		/datum/design/autolathe/circuit/reagentgrinder,
+		/datum/design/autolathe/circuit/industrialgrinder = 2,
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/conveyors
+	disk_name = "Technomancers LAT-018 Logistics"
+	icon_state = "technomancers"
+	rarity_value = 4
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	license = 10
+	designs = list(
+		/datum/design/autolathe/conveyor = 0,
+		/datum/design/autolathe/conveyor_switch = 0,
+		/datum/design/autolathe/circuit/smelter = 4,
+		/datum/design/autolathe/circuit/sorter = 2
+	)
+

Occulus-Server/Occulus-Eris

code/game/objects/items/weapons/design_disks/ironhammer.dm
@@ -1,0 +1,75 @@
+// Ironhammer
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/security
+	disk_name = "Ironhammer Miscellaneous Pack"
+	icon_state = "ironhammer"
+	rarity_value = 5
+	license = 20
+	designs = list(
+		/datum/design/autolathe/sec/secflashlight,
+		/datum/design/research/item/flash,
+		/datum/design/autolathe/sec/handcuffs,
+		/datum/design/autolathe/sec/zipties,
+		/datum/design/autolathe/sec/electropack,
+		/datum/design/autolathe/misc/taperecorder,
+		/datum/design/autolathe/tool/tacknife,
+		/datum/design/autolathe/sec/beartrap,
+		/datum/design/autolathe/sec/silencer,
+		/datum/design/autolathe/sec/hailer
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/generic
+	disk_name = "Ironhammer Combat Equipment - Standard Armor"
+	icon_state = "ironhammer"
+	spawn_tags = SPAWN_TAG_DESIGN_ADVANCED_COMMON
+	rarity_value = 12 // one of the more common advanced disks
+	license = 6 // 6 pieces, or 3 sets if you use helm + vest
+	designs = list(
+		/datum/design/autolathe/clothing/generic_helmet_basic,
+		/datum/design/autolathe/clothing/generic_vest,
+		/datum/design/autolathe/clothing/generic_vest_full = 2
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/generic/bulletproof
+	disk_name = "Ironhammer Combat Equipment - Bulletproof Armor"
+	icon_state = "ironhammer"
+	spawn_tags = SPAWN_TAG_DESIGN_ADVANCED_COMMON
+	rarity_value = 15 // about as rare as a advanced tool disk - remember that this takes from the 'advanced' pool (which is rare) instead of the 'common' pool like the normal armor disk does
+	license = 4 // 4 pieces, or 2 sets
+	designs = list(
+		/datum/design/autolathe/clothing/bulletproof_helmet_generic,
+		/datum/design/autolathe/clothing/bulletproof_vest_generic,
+		/datum/design/autolathe/clothing/bulletproof_vest_generic_full = 2
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/generic/ablative
+	disk_name = "Ironhammer Combat Equipment - Laserproof Armor"
+	icon_state = "ironhammer"
+	spawn_tags = SPAWN_TAG_DESIGN_ADVANCED_COMMON
+	rarity_value = 16 // slightly rarer than bulletproof gear
+	license = 4 // 4 pieces, or 2 sets
+	designs = list(
+		/datum/design/autolathe/clothing/ablative_vest,
+		/datum/design/autolathe/clothing/ablative_helmet
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/ih
+	disk_name = "Ironhammer Combat Equipment - Operator Armor"
+	icon_state = "ironhammer"
+	spawn_blacklisted = TRUE //should only be obtainable from the sectech
+	license = 6
+	designs = list(
+		/datum/design/autolathe/clothing/ih_helmet_basic,
+		/datum/design/autolathe/clothing/ih_vest_basic,
+		/datum/design/autolathe/clothing/ih_vest_basic_full = 2
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/armor/ih/bulletproof
+	disk_name = "Ironhammer Combat Equipment - Bulletproof Operator Armor"
+	icon_state = "ironhammer"
+	spawn_blacklisted = TRUE
+	license = 4
+	designs = list(
+		/datum/design/autolathe/clothing/ih_helmet_full,
+		/datum/design/autolathe/clothing/ih_vest_full
+	)
+

code/game/objects/items/weapons/design_disks/moebius.dm
@@ -1,0 +1,83 @@
+// Moebius
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/medical
+	disk_name = "Moebius Medical Designs"
+	icon_state = "moebius"
+	rarity_value = 4.5
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	license = 20
+	designs = list(
+		/datum/design/autolathe/gun/syringe_gun,
+		/datum/design/autolathe/misc/penflashlight,
+		/datum/design/autolathe/tool/scalpel,
+		/datum/design/autolathe/tool/circularsaw,
+		/datum/design/autolathe/tool/surgicaldrill,
+		/datum/design/autolathe/tool/retractor,
+		/datum/design/autolathe/tool/cautery,
+		/datum/design/autolathe/tool/hemostat,
+		/datum/design/autolathe/tool/bonesetter,
+		/datum/design/autolathe/container/syringe,
+		/datum/design/autolathe/container/vial,
+		/datum/design/autolathe/container/beaker,
+		/datum/design/autolathe/container/beaker_large,
+		/datum/design/autolathe/container/pill_bottle,
+		/datum/design/autolathe/container/spray,
+		/datum/design/autolathe/container/freezer_medical,
+		/datum/design/autolathe/device/implanter,
+		/datum/design/autolathe/container/syringegun_ammo,
+//		/datum/design/autolathe/container/syringe/large, Occulus Edit Not ported
+
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/surgery
+	disk_name = "Back Alley Organs"
+	icon_state = "moebius"
+	license = 5
+	designs = list(
+		/datum/design/bioprinter/lungs
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/computer
+	disk_name = "Moebius Computer Parts"
+	icon_state = "moebius"
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	rarity_value = 4.5
+	license = 20
+	designs = list(
+		/datum/design/autolathe/computer_part/frame_pda,
+		/datum/design/autolathe/computer_part/frame_tablet,
+		/datum/design/autolathe/computer_part/frame_laptop,
+		/datum/design/research/item/computer_part/disk/micro,
+		/datum/design/research/item/computer_part/disk/small,
+		/datum/design/research/item/computer_part/disk/normal,
+		/datum/design/research/item/computer_part/disk/advanced,
+		/datum/design/research/item/computer_part/cpu/basic,
+		/datum/design/research/item/computer_part/cpu/basic/small,
+		/datum/design/research/item/computer_part/cpu/adv,
+		/datum/design/research/item/computer_part/cpu/adv/small,
+		/datum/design/research/item/computer_part/netcard/basic,
+		/datum/design/research/item/computer_part/netcard/advanced,
+		/datum/design/research/item/computer_part/netcard/wired,
+		/datum/design/research/item/computer_part/cardslot,
+		/datum/design/research/item/computer_part/teslalink,
+		/datum/design/research/item/computer_part/portabledrive/basic,
+		/datum/design/research/item/computer_part/portabledrive/normal,
+		/datum/design/research/item/computer_part/printer,
+		/datum/design/research/item/computer_part/led,
+		/datum/design/research/item/computer_part/led/adv,
+		/datum/design/autolathe/computer_part/gps,
+		/datum/design/autolathe/computer_part/scanner/paper,
+		/datum/design/autolathe/computer_part/scanner/atmos,
+		/datum/design/autolathe/computer_part/scanner/reagent,
+		/datum/design/autolathe/computer_part/scanner/medical,
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/guns/ms_dartgun
+	disk_name = "Moebius Scientifica - Z-H P Artemis Dartgun"
+	icon_state = "moebius"
+	spawn_tags = SPAWN_TAG_DESIGN_ADVANCED
+	rarity_value = 50
+	license = 12
+	designs = list(
+		/datum/design/autolathe/gun/dart_gun = 3, // Z-H P Artemis"
+		/datum/design/autolathe/ammo/dart_mag,
+	)

code/game/objects/items/weapons/design_disks/technomancers.dm
@@ -1,0 +1,94 @@
+// Technomancers
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/components
+	disk_name = "Technomancers ARK-034 Components"
+	icon_state = "technomancers"
+	rarity_value = 4
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	license = 20
+	designs = list(
+		/datum/design/autolathe/part/consolescreen,
+		/datum/design/research/item/part/smes_coil,
+		/datum/design/research/item/part/basic_capacitor,
+		/datum/design/research/item/part/basic_sensor,
+		/datum/design/research/item/part/micro_mani,
+		/datum/design/research/item/part/basic_micro_laser,
+		/datum/design/research/item/part/basic_matter_bin,
+		/datum/design/autolathe/part/igniter,
+		/datum/design/autolathe/part/signaler,
+		/datum/design/autolathe/part/sensor_infra,
+		/datum/design/autolathe/part/timer,
+		/datum/design/autolathe/part/voice_analyzer,
+		/datum/design/autolathe/part/sensor_prox,
+		/datum/design/autolathe/part/camera_assembly,
+		/datum/design/autolathe/part/laserguide,
+		/datum/design/research/item/ducts
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/adv_tools
+	disk_name = "Technomancers IJIRO-451 Advanced Tools"
+	icon_state = "technomancers"
+	rarity_value = 11
+	spawn_tags = SPAWN_TAG_DESIGN_ADVANCED_COMMON
+	license = 10
+	designs = list(
+		/datum/design/autolathe/tool/big_wrench,
+		/datum/design/autolathe/tool/pneumatic_crowbar,
+		/datum/design/research/item/weapon/mining/jackhammer,
+		/datum/design/research/item/weapon/mining/drill,
+		/datum/design/research/item/weapon/mining/drill_diamond,
+		/datum/design/autolathe/tool/pickaxe_excavation,
+		/datum/design/autolathe/tool/shovel/power,
+		/datum/design/autolathe/tool/circularsaw,
+		/datum/design/autolathe/tool/powered_hammer,
+		/datum/design/autolathe/tool/chainsaw,
+		/datum/design/autolathe/tool/hypersaw,
+		/datum/design/autolathe/tool/rcd,
+		/datum/design/autolathe/tool/electric_screwdriver,
+		/datum/design/autolathe/tool/combi_driver,
+		/datum/design/autolathe/tool/armature_cutter,
+		/datum/design/autolathe/tool/weldertool_adv,
+		/datum/design/autolathe/part/diamondblade,
+		/datum/design/autolathe/tool/rpd,
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/circuits
+	disk_name = "Technomancers ESPO-830 Circuits"
+	icon_state = "technomancers"
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	rarity_value = 5.5
+	license = 10
+	designs = list(
+		/datum/design/autolathe/circuit/airlockmodule = 0,
+		/datum/design/autolathe/circuit/airlockmodule/secure,
+		/datum/design/autolathe/circuit/airalarm = 0,
+		/datum/design/autolathe/circuit/firealarm = 0,
+		/datum/design/autolathe/circuit/powermodule = 0,
+		/datum/design/autolathe/circuit/recharger,
+		/datum/design/research/circuit/autolathe,
+		/datum/design/autolathe/circuit/autolathe_disk_cloner = 3,
+		/datum/design/autolathe/circuit/vending,
+		/datum/design/research/circuit/arcade_battle,
+		/datum/design/research/circuit/arcade_orion_trail,
+		/datum/design/research/circuit/teleconsole,
+		/datum/design/research/circuit/operating,
+		/datum/design/autolathe/circuit/helm,
+		/datum/design/autolathe/circuit/nav,
+		/datum/design/autolathe/circuit/centrifuge,
+		/datum/design/autolathe/circuit/electrolyzer,
+		/datum/design/autolathe/circuit/reagentgrinder,
+		/datum/design/autolathe/circuit/industrialgrinder = 2,
+	)
+
+/obj/item/weapon/computer_hardware/hard_drive/portable/design/conveyors
+	disk_name = "Technomancers LAT-018 Logistics"
+	icon_state = "technomancers"
+	rarity_value = 4
+	spawn_tags = SPAWN_TAG_DESING_COMMON
+	license = 10
+	designs = list(
+		/datum/design/autolathe/conveyor = 0,
+		/datum/design/autolathe/conveyor_switch = 0,
+		/datum/design/autolathe/circuit/smelter = 4,
+		/datum/design/autolathe/circuit/sorter = 2
+	)
+