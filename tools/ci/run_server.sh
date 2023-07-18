be0422dbda4f426b6d21d2bf0999bc5e7aa86fd9
Jamini <mobiousjami@gmail.com>
2023-07-18 18:05:53 -0400

    Revert "Fix CI config, again"

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -4,10 +4,8 @@ set -euo pipefail
 tools/deploy.sh ci_test
 # mkdir ci_test/config
 
-# run with example config to make sure that the configuration absolutely works as intended
-cp -p config/example/* ci_test/config/
-# test config
-cp -p tools/ci/ci_config.txt ci_test/config/config.txt
+#test config
+cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
 DreamDaemon cev_eris.dmb -close -trusted -verbose -params "log-directory=ci"
